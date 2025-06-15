e93e9600b709412d3f6f4bdaa9fcdf79a799dd6b
Maelton Lima <maelton.main@gmail.com>
2025-06-15 16:03:49 -0300

    fix: add prisma/generated to dist/

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,5 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { UserRepository } from './user.repository';
-import { User } from '@prisma-generated/client';
 
 @Injectable()
 export class UserService {
