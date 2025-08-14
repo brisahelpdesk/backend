78c5ee57a85b2673dbbbf835782543f0cd583127
Maelton Lima <maelton.main@gmail.com>
2025-08-14 10:49:28 -0300

    chore: comment auth module for testing

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,10 +1,9 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { UserModule } from '../user/user.module';
-
-@Module({
-    imports: [UserModule], // Import UserModule to use UserService
-    providers: [AuthService],
-    exports: [AuthService], // Export AuthService so other modules can use it
-})
-export class AuthModule {}
+// import { Module } from '@nestjs/common';
+// import { AuthService } from './auth.service';
+//
+// @Module({
+//     imports: [UserModule], // Import UserModule to use UserService
+//     providers: [AuthService],
+//     exports: [AuthService], // Export AuthService so other modules can use it
+// })
+// export class AuthModule {}
